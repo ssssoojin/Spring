db65ce442bdf4ab4fa5f478c34e1aa086c1a3df7
soojin66 <rlatnwls0606@naver.com>
2021-12-27 12:07:37 +0900

    2021.12.27 스프링 v1

---
+++ src/test/java/org/conan/test/JDBCTest.java
@@ -0,0 +1,33 @@
+package org.conan.test;
+
+import static org.junit.Assert.fail;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import org.junit.Test;
+
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+public class JDBCTest {
+	static {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (Exception e) {
+		}
+	}
+
+	@Test
+	public void testConnection() {
+		try (Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/scottdb?useSSL=false", "scott",
+				"tiger")) {
+			log.info(con);
+		}
+
+		catch (Exception e) {
+			fail(e.getMessage());
+		}
+	}
+
+}
