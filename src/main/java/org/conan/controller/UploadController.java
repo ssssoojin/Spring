0497730c716d1e3b9cbc80c106538f90d9ab9ad9
수진 <rlatnwls0606@gmail.com>
2022-01-05 15:32:13 +0900

    2022.01.05 파일업로드(중복된 이름의 파일 처리-1)

--- src/main/java/org/conan/controller/UploadController.java
+++ src/main/java/org/conan/controller/UploadController.java
@@ -1,6 +1,8 @@
 package org.conan.controller;
 
 import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -27,32 +29,49 @@ public class UploadController {
 			log.info("Upload File Size : " + multipartFile.getSize());
 
 			File saveFile = new File(uploadFolder, multipartFile.getOriginalFilename());
-			try { 
+			try {
 				multipartFile.transferTo(saveFile);
-			} catch (Exception e) { 
+			} catch (Exception e) {
 				log.error(e.getMessage());
 			}
 		}
 	}
+
 	@GetMapping("/uploadAjax")
 	public void uploadAjax() {
 		log.info("upload ajax");
 	}
+
 	@PostMapping("uploadAjaxAction")
 	public void uploadAjaxPost(MultipartFile[] uploadFile) {
 		String uploadFolder = "e:/spring/upload";
+		// 폴더 생성하기
+				File uploadPath = new File(uploadFolder, getFolder());
+				log.info("uploadPath : " +uploadPath);
+				if (uploadPath.exists() == false) {//폴더가 존재하지 않으면 새로 생성
+					uploadPath.mkdirs(); // yyyy/MM/dd 폴더 생성
+				}
+				
 		for (MultipartFile multipartFile : uploadFile) {
 			log.info("--------------------------------------");
 			log.info("Upload File Name : " + multipartFile.getOriginalFilename());
 			log.info("Upload File Size : " + multipartFile.getSize());
 
-			File saveFile = new File(uploadFolder, multipartFile.getOriginalFilename());
-			try { 
+			File saveFile = new File(uploadPath, multipartFile.getOriginalFilename());
+			try {
 				multipartFile.transferTo(saveFile);
-			} catch (Exception e) { 
+			} catch (Exception e) {
 				log.error(e.getMessage());
 			}
 		}
 	}
-	
+
+	// 오늘 날짜의 경로를 문자열로 생성한다.
+	private String getFolder() {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		Date date = new Date();
+		String str = sdf.format(date);
+		return str.replace("-", File.separator);
+	}
+
 }
